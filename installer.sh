11c97f4be741fb5a8a1cce25337c2aaaed5f07e8
DeadHacker <aitor.rosello11@gmail.com>
2022-05-01 14:42:59 -0400

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,50 @@
+#!bin/bash
+
+clear
+echo
+echo "                           _____           _       _   _       _   _                  "
+echo "                          |_   _|         | |     (_) | |     (_) | |                 "
+echo "                            | |    _ __   | |__    _  | |__    _  | |_    ___    _ __ "
+echo "                            | |   | '_ \  | '_ \  | | | '_ \  | | | __|  / _ \  | '__|"
+echo "                           _| |_  | | | | | | | | | | | |_) | | | | |_  | (_) | | |   "
+echo "                          |_____| |_| |_| |_| |_| |_| |_.__/  |_|  \__|  \___/  |_|   " 
+echo "                              __________________________________________________"					
+echo "                                ︻デ═一  Created by: XDeadHackerX v1.1  ︻デ═一 " 
+echo "          -------------------------------------------------------------------------------------------"
+echo "          Cualquier acción y o actividad relacionada con Inhibitor es únicamente su responsabilidad"
+echo "          -------------------------------------------------------------------------------------------"
+echo "               Any action and or activity related to Inhibitor is solely your responsibility"
+echo "          -------------------------------------------------------------------------------------------"
+echo
+echo
+echo "[*] Elige idioma / Select language"
+echo
+echo "#####################"
+echo "[1] Spain/es"
+echo "[2] English/en"
+echo "[3] Exit"
+echo "#####################"
+echo
+echo
+read -p "[*] Elige una opcion / Choose one option: " opc
+	case $opc in
+			1 )	echo
+				cp requisitos/es/inhibitor.sh .
+				;;
+			2 )	echo
+				cp requisitos/en/inhibitor.sh .
+				;;
+			3 )	exit
+				;;
+			* )	echo
+				echo "$RRPLY No es una opcion valida / It is not a valid option"
+				sleep 3
+				bash installer.sh
+	esac
+	
+sudo apt install hping3	
+sudo apt install aircrack-ng
+sudo apt-get install mdk4
+sudo apt-get install -y netdiscover
+chmod 777 inhibitor.sh
+bash inhibitor.sh
